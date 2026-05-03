9a3f5ef34557560dac11ec1281e43002d96ab976
Juan Vidal Pacheco <juavidal@gmail.com>
2026-05-02 21:22:54 -0400

    fix: reset migrations and switch to db push for mysql

--- backend/prisma/migrations/20260409021354_link_funcionario_to_centro_fixed/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Funcionario" ADD COLUMN     "centro_salud_id" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Funcionario" ADD CONSTRAINT "Funcionario_centro_salud_id_fkey" FOREIGN KEY ("centro_salud_id") REFERENCES "CentroSalud"("id") ON DELETE SET NULL ON UPDATE CASCADE;
