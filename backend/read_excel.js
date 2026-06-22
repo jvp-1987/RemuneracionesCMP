0214b0a2e3a1344c606cf2a75ec8b273db098a1a
Juan Vidal Pacheco <juavidal@gmail.com>
2026-06-21 22:58:49 -0400

    Update read_excel.js

--- backend/read_excel.js
+++ backend/read_excel.js
@@ -1,12 +1,32 @@
 const XLSX = require('xlsx');
-const workbook = XLSX.readFile('../Planilla Global CAS SALUD MARZO.xlsx');
-const sheetName = workbook.SheetNames[0];
-const sheet = workbook.Sheets[sheetName];
-const data = XLSX.utils.sheet_to_json(sheet, { header: 1, range: 0 });
-console.log("Sheet Name:", sheetName);
-console.log("Row 0:", data[0]);
-console.log("Row 1:", data[1]);
-console.log("Row 2:", data[2]);
-console.log("Row 3:", data[3]);
-console.log("Row 4:", data[4]);
-console.log("Row 5:", data[5]);
+const path = require('path');
+const file = path.join(__dirname, '../MAESTRO SALUD Marzo  2026.xlsx');
+const workbook = XLSX.readFile(file);
+const haberesSheet = workbook.Sheets['Haberes'] || workbook.Sheets['haberes'];
+if (!haberesSheet) {
+  console.log("No Haberes sheet found.");
+} else {
+  const data = XLSX.utils.sheet_to_json(haberesSheet);
+  const targetRuts = ["10765330-4", "17227257-1", "18131174-1", "15266879-0", "17329089-9"];
+  const normalizeRut = (rut) => {
+    if (!rut) return null;
+    let str = String(rut).trim().toUpperCase();
+    str = str.replace(/\./g, '');
+    str = str.replace(/^0+/, '');
+    return str;
+  };
+  data.forEach((row, i) => {
+    const rut = normalizeRut(row['RUT']);
+    if (targetRuts.includes(rut)) {
+      console.log(`Found RUT ${rut} in Excel row ${i}:`, {
+        'SUELDO BASE': row['SUELDO BASE'],
+        'HORAS EXTRAS 25%': row['HORAS EXTRAS 25%'],
+        'HORAS EXTRAS 50%': row['HORAS EXTRAS 50%'],
+        'CANT. H.E. 25%': row['CANT. H.E. 25%'],
+        'CANT. H.E. 50%': row['CANT. H.E. 50%']
+      });
+    }
+  });
+}
+
+
