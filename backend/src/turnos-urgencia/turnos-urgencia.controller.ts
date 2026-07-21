fa31bedefb14ec0c7f9950006f5895d1e09eb023
Juan Vidal Pacheco <juavidal@gmail.com>
2026-07-21 18:39:53 -0400

    Update turnos-urgencia.controller.ts

--- backend/src/turnos-urgencia/turnos-urgencia.controller.ts
+++ backend/src/turnos-urgencia/turnos-urgencia.controller.ts
@@ -29,19 +29,19 @@ export class TurnosUrgenciaController {
   }
 
   @Patch(':id')
-  @Roles('ADMIN', 'ADMIN_MAESTRO', 'CONTROL', 'FINANZAS', 'CONTABILIDAD', 'CENTRO_SALUD', 'SECRETARIA')
+  @Roles('ADMIN', 'ADMIN_MAESTRO', 'CONTROL', 'FINANZAS', 'CONTABILIDAD', 'CENTRO_SALUD', 'SECRETARIA', 'VALIDADOR_CONVENIOS')
   update(@Param('id') id: string, @Body() dto: UpdateTurnoUrgenciaDto, @Req() req: any) {
     return this.turnosUrgenciaService.update(+id, dto, req.user);
   }
 
   @Patch('bulk/:consolidadoId')
-  @Roles('ADMIN', 'ADMIN_MAESTRO', 'CONTROL', 'FINANZAS', 'CONTABILIDAD', 'CENTRO_SALUD', 'SECRETARIA')
+  @Roles('ADMIN', 'ADMIN_MAESTRO', 'CONTROL', 'FINANZAS', 'CONTABILIDAD', 'CENTRO_SALUD', 'SECRETARIA', 'VALIDADOR_CONVENIOS')
   bulkUpdate(@Param('consolidadoId') consolidadoId: string, @Body() dto: UpdateTurnoUrgenciaDto, @Req() req: any) {
     return this.turnosUrgenciaService.bulkUpdate(+consolidadoId, dto, req.user);
   }
 
   @Delete(':id')
-  @Roles('ADMIN', 'ADMIN_MAESTRO', 'CONTROL', 'FINANZAS', 'CONTABILIDAD', 'CENTRO_SALUD', 'SECRETARIA')
+  @Roles('ADMIN', 'ADMIN_MAESTRO', 'CONTROL', 'FINANZAS', 'CONTABILIDAD', 'CENTRO_SALUD', 'SECRETARIA', 'VALIDADOR_CONVENIOS')
   remove(@Param('id') id: string, @Req() req: any) {
     return this.turnosUrgenciaService.remove(+id, req.user);
   }
